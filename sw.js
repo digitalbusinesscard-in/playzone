77931aa09de7bff3559c69baae39596ee43d473f
digitalbusinesscard-in <121642931+digitalbusinesscard-in@users.noreply.github.com>
2023-01-15 01:03:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener ("fetch", async (event) => {
  });
 
  const cacheName = "pwa-conf-v1";
- const staticAssets = ["./", "./index.html", "./app.js", "./style.css", "./img/shoe.svg", "./img/bg.png", "./img/boot.png", "./img/facebook.png', "./img/google-pay.png', "./img/insta.svg', "./img/instagram.png', "./img/logo.png', "./img/phone.svg', "./img.-logo.png', "./img/share.png', "./img/site.svg', "./img/verified.svg', "./img/whatsapp.svg', "./img/whatsapp.png'];
+ const staticAssets = ["./", "./index.html", "./app.js", "./style.css", "./img/shoe.svg", "./img/bg.png", "./img/boot.png", "./img/facebook.png", "./img/google-pay.png", "./img/insta.svg", "./img/instagram.png", "./img/logo.png", "./img/phone.svg", "./img.-logo.png", "./img/share.png", "./img/site.svg", "./img/verified.svg", "./img/whatsapp.svg", "./img/whatsapp.png"];
 
 self.addEventListener("install", async (event) => {
    const cache = await caches.open(cacheName);
