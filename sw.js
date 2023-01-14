42ae78a7a24dba3799800d264928d32ac3c79659
digitalbusinesscard-in <121642931+digitalbusinesscard-in@users.noreply.github.com>
2023-01-15 01:02:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const cacheName = 'pwa-cofnf-v1';
-const staticAssets = ['./', './index.html', './app.js', './style.css', './img/shoe.svg', './img/bg.png', './img/boot.png', './img/facebook.png', './img/google-pay.png', './img/insta.svg', './img/instagram.png', './img/logo.png', './img/phone.svg', './img.-logo.png', './img/share.png', './img/site.svg', './img/verified.svg', './img/whatsapp.svg', './img/whatsapp.png'];
+self.addEventListener ("install", async (event) => {
+    console.log("install event");
+   });
 
-self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName); 
-  await cache.addAll(staticAssets); 
-});
+self.addEventListener ("fetch", async (event) => {
+  console.log("fetch event");
+ });
 
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  event.respondWith(cacheFirst(req));
-});
+ const cacheName = "pwa-conf-v1";
+ const staticAssets = ["./", "./index.html", "./app.js", "./style.css", "./img/shoe.svg", "./img/bg.png", "./img/boot.png", "./img/facebook.png', "./img/google-pay.png', "./img/insta.svg', "./img/instagram.png', "./img/logo.png', "./img/phone.svg', "./img.-logo.png', "./img/share.png', "./img/site.svg', "./img/verified.svg', "./img/whatsapp.svg', "./img/whatsapp.png'];
 
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName); 
-  const cachedResponse = await cache.match(req); 
-  return cachedResponse || fetch(req); 
-};
+self.addEventListener("install", async (event) => {
+   const cache = await caches.open(cacheName);
+   await cache.addAll (staticAssets);
+ });
